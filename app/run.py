21fedec066a2d61da8ee3cccca63df9f6b0831e5
passl <30534845+pschropp@users.noreply.github.com>
2022-10-21 11:25:39 +0200

    Update run.py

--- app/run.py
+++ app/run.py
@@ -8,7 +8,7 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
 
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/disaster_response.db')
+df = pd.read_sql_table('messages', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -43,6 +43,10 @@ def index():
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
+    cat_count = df.drop(['message', 'original', 'genre'], axis=1).sum(axis=0).sort_values(ascending=False)
+    percentage=cat_count
+    percentage['percent'] = round(cat_count / df.shape[0] * 100).apply(lambda x: str(x) + '%')
+        
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -61,6 +65,24 @@ def index():
                 },
                 'xaxis': {
                     'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=cat_count.index.values,
+                    y=cat_count.values,
+                    text=percentage['percent']
+                )
+            ],
+            'layout': {
+                'title': 'Count (+ Percentage) of Categorizations in all messages',
+                'yaxis': {
+                    'title': "Count of Categorizations"
+                },
+                'xaxis': {
+                    'title': "Category"
                 }
             }
         }
@@ -82,7 +104,8 @@ def go():
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
-    classification_results = dict(zip(df.columns[4:], classification_labels))
+    classification_results = dict(zip(df.columns[3:], classification_labels))
+    print(classification_results)
 
     # This will render the go.html Please see that file. 
     return render_template(
@@ -93,7 +116,7 @@ def go():
 
 
 def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
 
 
 if __name__ == '__main__':
